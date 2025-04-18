bd7a64a81311c09c9a7aa1de0bf807f8ef0b1b9e
EugeneLevkovych <eugenelevkovych@yahoo.com>
2025-04-18 07:05:35 +0300

    mod conf

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import './App.css'
-import './index.css'
-import Header from './components/Header'
-import Menu from './components/Menu'
-import Footer from './components/Footer'
+import "./App.css";
+import "./index.css";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className='container'>
-    <Header />
-    <Menu />
-    <Footer />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
-  )
+  );
 }
-
-
-
- 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,21 @@
 export default function Footer() {
-   const hour = new Date().getHours()
-   const openHour = 12
-   const closeHour = 23
-   const isOpen = hour >= openHour && hour <= closeHour
-   // console.log(isOpen);
-   
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 23;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  // console.log(isOpen);
 
-   // if(hour >= openHour && hour <= closeHour)alert("We're currently open");
-   //  else alert("Sorry we're closed");
-   
-   return <footer className="footer">{isOpen && (<div className="order">
-       <p>We're open until {closeHour}: 00.Come visit us or order online.</p>
-       <button className="btn">Order</button>
-   </div>)}</footer> 
+  // if(hour >= openHour && hour <= closeHour)alert("We're currently open");
+  //  else alert("Sorry we're closed");
+
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}: 00.Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  );
 }
